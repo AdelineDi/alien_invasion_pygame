a21d82856aefebecec750128b982d06174080b7c
Darya Zenina <dariadasha13@gmail.com>
2025-10-14 19:41:23 +0300

    Next next commit

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,9 @@
-import sys
 import pygame
 from settings import Settings
 from ship import  Ship
 import game_functions as gf
+from pygame.sprite import Group
+
 
 def run_game():
     """ Инициализирует игру """
@@ -12,10 +13,13 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
     ship = Ship(ai_settings ,screen)
+    #Создание группы для хранения пуль
+    bullets = Group()
     #bg_colour = (216, 187, 237)
     while True:
         # Отслеживание событий клавиатуры и мыши.
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 run_game()
--- settings.py
+++ settings.py
@@ -8,3 +8,9 @@ class Settings():
         #self.bg_colour = (216, 187, 237)
         self.bg_colour = (230, 230, 230)
         self.ship_speed_factor = 1.5
+
+        #Параметры пули
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_colour = 60, 60, 60
